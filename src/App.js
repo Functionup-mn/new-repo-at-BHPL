dfce9ccc07be52d972769b01e2c0282aac7e4d8a
Functionup-mn <nadeem2mau@gmail.com>
2023-03-07 02:04:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer'
+import Student from './Components/Student';
+import movies from './movies.json'
+import Movie from './Components/Movie';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      {/* <Student name="MNadeem" age={23}/>
+      <Student name="MAbdullah" age={34}/>
+      <Footer/> */}
+      <div className="main">
+        {
+         movies.map((element, index) => {
+          return(                                                   
+             <Movie key={index}
+                    title={element.Title}
+                    year={element.Year}
+                    time={element.Runtime}
+                    img={element.Poster}
+              />
+          )
+         })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Components/Student.js
@@ -0,0 +1,11 @@
+
+const Student = (props) => {
+  return (
+    <div>
+     <h1>{props.name}</h1> 
+      <h2>{props.age}</h2>
+    </div>
+  )
+}
+
+export default Student
