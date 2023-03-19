d9b5af2a0ade33a07ed9f05a9f944671ffd32993
Functionup-mn <nadeem2mau@gmail.com>
2023-03-20 04:10:40 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,82 +1,41 @@
 import './App.css';
 import Header from './Components/Header';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import Stack from '@mui/material/Stack';
-import AddIcon from '@mui/icons-material/Add'
-import { useState } from 'react';
-import Fields from './Components/Fields';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {useState, useEffect} from 'react';
 
 function App() {
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
+  const [state, setState] = useState(10)
   const [data, setData] = useState([])
 
-  const addData = () => {
-    setData([...data,{
-      name: name,
-      email: email
-  }])
-  setName("")
-  setEmail("")
-}
-  // To remove data
-const removeItem = (index) => {
-   let arr = data
-   arr.splice(index, 1)
-   setData([...arr])
-}
+  useEffect(() => {
+    async function getData(){
+      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`)
+      const res = await get.json()
+      setData(res)
+    }
+    getData()
+
+    document.title = `(${state}) Emplyes Online`
+  }, [state])  // this is called  dependencies
+
 
   return (
-    <div className="MyApp">
-      <Header/>
-      {/* Form  */}
-      <div className='form'>
-       <Stack direction ="row" spacing={2}> 
-        <TextField 
-          value={name} 
-          onChange = {(event) => setName(event.target.value)}
-          id="outlined-basic" 
-          label="Name" 
-          variant="outlined" />
-        <TextField 
-          value={email} 
-          onChange = {(event) => setEmail(event.target.value)}
-          id="outlined-basic" 
-          label="email" 
-          variant="outlined" />
-        <Button onClick={addData} variant="contained" color="success">
-           <AddIcon/>
-      </Button>
-      </Stack>
-      </div>
-      {/* Data */}
-      <div className='data'>
-      <div className='data_val'>
-            <h3>Name</h3>
-            <h3>Email</h3>
-            <h3>Remove</h3>
-      </div>
+      <div>
+        <Header/>
+        <button onClick={() => setState(state+5)}>Click Me {state}</button>
         {
           data.map((element, index) => {
-            return(
-              <div key={index} className="data_val">
-              <h3>{element.name}</h3>
-              <h3>{element.email}</h3>
-              <Stack>
-                <Button onClick={()=>removeItem(index)} variant="outlined" color="error">
-                    <DeleteIcon/>
-                </Button>
-              </Stack>
+           return( 
+            <div className='data' key={index}>
+              <h5>{element.firstName}</h5>
+              <h5>{element.lastName}</h5>
+              <h5>{element.email}</h5>
             </div>
-            )
+           )
           })
         }
       </div>
-    </div>
-  );
+  )
 }
 
 export default App;
