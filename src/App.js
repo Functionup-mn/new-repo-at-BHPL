1580104c9fe11e34bc0fe7e09a6f6d6f37dfe905
Functionup-mn <nadeem2mau@gmail.com>
2023-03-27 01:13:37 +0530

    we learned about how to use custom hook , through this we make the code reusability like function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import React, { useReducer } from 'react';
+
 import './App.css';
 import Header from './Component/Header';
-
-const reducer = (state, action) =>{
-  if(action.type === 'INC'){
-    return state + 1
-  }else if(action.type === 'DEC'){
-    return state - 1 
-  }else if(action.type === 'MUL'){
-    return state * 2
-  }else{
-   return state
-}
-}
+import useFetch from './Component/useFetch';
 
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, 0)  // reducer just a name , 0 is initial state
-
+     const [data] = useFetch("https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001")
+    
   return (
     <div className='myApp'>
     <Header/>
-    <h1>{state}</h1>
-    <button onClick={() => dispatch({type: "INC"})}>Increment</button>
-    <button onClick={() => dispatch({type: "DEC"})}>Decrement</button>
-    <button onClick={() => dispatch({type: "MUL"})}>Multiply</button>
+    {data.map((event, index) =>{
+      return <h1 key={index}>{event.firstName}</h1>
+    })}
     </div>
   )
 }
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-      <h2>useReducer Hook</h2>
+      <h2>Custom Hook</h2>
     </div>
   )
 }
