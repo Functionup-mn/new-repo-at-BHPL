b7c3b598d5dacda6ba089617424f6c3082d58e3a
Functionup-mn <nadeem2mau@gmail.com>
2023-08-19 22:36:49 +0530

    high order component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css'
 
 
-function App(){
-                                      // UnControlled components: when input fields are not controlled through the state in React then this components are called as uncontrolled components. this is controlled through javascript or Ref.
-     let inputRef = useRef(null)
-     let inputRef2 = useRef(null)
-
-   function submitForm(e){
-        e.preventDefault()
-        console.log("Input field 1:", inputRef.current.value);
-        console.log("input field 2:", inputRef2.current.value);
-        let Input3 = document.getElementById("input3").value;
-        console.log("Input field 3:", Input3);
-   }
-
+function App(){            
+                            // Higher-order components or HOC is the advanced method of reusing the component functionality logic. It simply takes the original component and returns the enhanced component.
+ 
   return (
     <div className='App'>
-      <h1>  UnControled Component in React</h1> 
-      <form onSubmit={submitForm}>
-        <input ref={inputRef} type="text" />
-        <br />
-        <br />
-        <input ref={inputRef2} type="text" />
-        <br />
-        <br />
-        <input id='input3' type="text" />
-        <br />
-        <button>Submit</button>
-      </form>
+      <h1>  High Order Component in React</h1> 
+        <HOCRed cmp={Counter}/>
+        <HOCGreen cmp={Counter}/>
+        <HOCYellow cmp={Counter}/>
     </div>
   )
 }
 
-export default App;
+function HOCRed(props){
+  
+  return <h2 style={{backgroundColor: 'red', width:'300px'}}>Red<props.cmp/></h2>
+}
+
+function HOCGreen(props){
+  
+  return <h2 style={{backgroundColor: 'green', width:'300px'}}>Green<props.cmp/></h2>
+}
+function HOCYellow(props){
+  
+  return <h2 style={{backgroundColor: 'yellow', width:'300px'}}>Yellow<props.cmp/></h2>
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={(()=> setCount(count+1))}>Update Count</button>
+    </div>
+  )
+}
+
+export default App; 
