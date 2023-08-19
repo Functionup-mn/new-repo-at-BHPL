24b4fe19d1a621530f562c8db830ceb0494e790d
Functionup-mn <nadeem2mau@gmail.com>
2023-08-19 11:07:50 +0530

    uncontrolled component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './App.css'
 
 
 function App(){
-                                      // Controlled components: when input fields are controlled through the state in React then this components are called as controlled components.
-  const [val, setVal] = useState('');
-                                          
-  function handleSubmit(e){
-          setVal(e.preventDefault());
-  }
+                                      // UnControlled components: when input fields are not controlled through the state in React then this components are called as uncontrolled components. this is controlled through javascript or Ref.
+     let inputRef = useRef(null)
+     let inputRef2 = useRef(null)
+
+   function submitForm(e){
+        e.preventDefault()
+        console.log("Input field 1:", inputRef.current.value);
+        console.log("input field 2:", inputRef2.current.value);
+        let Input3 = document.getElementById("input3").value;
+        console.log("Input field 3:", Input3);
+   }
+
   return (
     <div className='App'>
-      <h1>Controled Component in React</h1> 
-      <h2>Value: {val}</h2>               
-      <input type="text" value={val} onChange={((e)=> setVal(e.target.value))} />
-      <button type='submit' onClick={handleSubmit}> Submit  </button>
+      <h1>  UnControled Component in React</h1> 
+      <form onSubmit={submitForm}>
+        <input ref={inputRef} type="text" />
+        <br />
+        <br />
+        <input ref={inputRef2} type="text" />
+        <br />
+        <br />
+        <input id='input3' type="text" />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
