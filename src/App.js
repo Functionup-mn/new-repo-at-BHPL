124e0baaaa7a2355f4239565463dbadad9dc920f
Functionup-mn <nadeem2mau@gmail.com>
2023-08-21 21:54:13 +0530

    I learned how to fetch the data using Api

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-} from "react-router-dom";
-import User from "./Components/User"
+import React, { useState, useEffect } from "react";
+
+import Table from 'react-bootstrap/Table';
 import "./App.css";
 
 function App() {
-  let Users = [
-    {id: 1, name: "John", email: "john@gmail.com", password: "123"},
-    {id: 2, name: "Peter", email: "peter@gmail.com", password: "103"},
-    {id: 3, name: "Adam", email: "adam@gmail.com", password: "128"},
-    {id: 4, name: "Michale", email: "michale@gmail.com", password: "183"},
-    {id: 5, name: "Henry", email: "henry@gmail.com", password: "675"},
-    {id: 14, name: "Julie", email: "julie@gmail.com", password: "163"},
-  ]
+  const [data, setData] = useState([]);
+ useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users').then((result)=>{
+      result.json().then((resp)=>{
+        // console.log(resp);
+        setData(resp);
+      })
+  })
+ })
   return (
     <div className="App">
-      <Router>
-        
-        <h1>Dynamic Routing</h1>
-         {
-          Users.map((item,i)=>
-             <div key={i}>
-              <Link to= {"/user/"+item.id +'/'+item.name}><h1>{item.name}</h1></Link>
-             </div>
-          )
-         }
-         <Routes>
-         <Route path="/user/:id/:name"><User/></Route>
-         </Routes>
-      </Router>
+      <h1>Get Api Call</h1>
+      <Table striped variant="dark">
+        <thead>
+          <tr>
+            <th>S.No.</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Username</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+             data.map((item)=>
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.username}</td>
+                <td>{item.website}</td>
+              </tr>
+             )
+          }
+        </tbody>
+      </Table>
     </div>
   );
 }
